1ba97d53b3138b411f9e00d5394f01b6683629ae
kakao <kakao@kakaoui-MacBookPro.local>
2022-08-05 19:40:26 +0900

    switching, 점검 api 추가

--- lumos.py
+++ lumos.py
@@ -1,10 +1,12 @@
+from ast import Pass
 from importlib.metadata import metadata
 from itertools import count
 import json
 from ssl import Options
+from threading import excepthook
 import uvicorn
 import fastapi
-from datetime import datetime
+from datetime import date, datetime
 from model import schema_api_model as schema
 from util.Logger import *
 from util.ExecSSH import *
@@ -13,6 +15,7 @@ from fastapi_utils.tasks import repeat_every
 import re
 from typing import Optional
 from util.func import *
+import os
 
 app = fastapi.FastAPI()
 
@@ -94,7 +97,108 @@ def read_customed(service_name: Optional[str]="%", issue_type: Optional[str]="%"
 # 	return True
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### 일단 vip 기반 mha 구성한 거에서 로직 짜고 있음
+### mha manager에 접속 및 switch shell script 실행
+@app.get('/lumos/switch_call', tags=["switch_call"])
+def switch_call():
+	now = datetime.datetime.today().strftime("%Y%m%d")
+	Log = Logger("./logs/{}.log".format(now), 0)
+	Log.write_log(3, "[LUMOS][Report] Call Received\n")	
+	shell = ExecSSH(host='mha-practice.ay1.krane.9rum.cc', user='deploy', logger=Log)
+
+	cmd=f"masterha_master_switch --master_state=alive --conf=/etc/masterha/app1.cnf --new_master_host=slave-practice --interactive=0 2> /dev/null"
+	exit_status, stdout, stderr = shell.exec_command(cmd, "deploy")
+	Log.write_log(3, "{}, {}, {}\n".format(str(exit_status), str(stdout), str(stderr)))
+	if "gtid inconsistency" in stdout:
+		return "switching is failed.\n because gtid inconsistency problem."
+	elif "known_hosts inconsistency" in stdout:
+		return "switching is failed.\n because known_hosts inconsistency."
+	elif "long query locked problem" in stdout:
+		return "switching is failed.\n because long query locked problem."
+	else:
+		return "switching is succeeded.\n"
+
+
+@app.get('/lumos/gtid_check', tags=["gtid_check"])
+def gtid_check():
+	now = datetime.datetime.today().strftime("%Y%m%d")
+	Log = Logger("./logs/{}.log".format(now), 0)
+	Log.write_log(3, "[LUMOS][Report] Call Received\n")	
+	dbconn1 = MysqlConnector('master-practice.ay1.krane.9rum.cc', 3306, 'analysis', 'mha', 'mha')
+	dbconn2 = MysqlConnector('slave-practice.ay1.krane.9rum.cc', 3306, 'analysis', 'mha', 'mha')
+
+	query1="show slave status;"
+	ret=dbconn1.execsql(query1)
+	
+	query2="show variables like 'server_uuid';"
+	master_uuid=""
+	if len(ret)==0:
+		master_uuid=dbconn1.execsql(query2)[0][1]
+	else:
+		master_uuid=dbconn2.execsql(query2)[0][1]
+	
+	query3="show variables like 'gtid_executed';"
+	gtid_sets1=sorted(dbconn1.execsql(query3)[0][1].split(','))
+	gtid_sets2=sorted(dbconn2.execsql(query3)[0][1].split(','))	
+	size=len(gtid_sets1)
+	for i in range(size):
+		if master_uuid in gtid_sets1[i]: # 현재 업데이트 되고 있는 gtid에 대해서 대량 쿼리로 인해 master, slave 간 동기화 상태가 불일치하는 상황은 예외처리 해주기
+			continue
+		if gtid_sets1[i]!=gtid_sets2[i]:
+			return 0
+	return 1
+
+
+# ssh 접속 전에 hosts 정보 삭제하도록 함
+@app.get('/lumos/known_hosts_check', tags=["known_hosts_check"])
+def known_hosts_check():
+	now = datetime.datetime.today().strftime("%Y%m%d")
+	Log = Logger("./logs/{}.log".format(now), 0)
+	Log.write_log(3, "[LUMOS][Report] Call Received\n")	
+
+	# os.system("sed '/master-practice/d' /home/deploy/.ssh/known_hosts")
+	# os.system("sed '/slave-practice/d' /home/deploy/.ssh/known_hosts")
+	os.system("sed -i '' '/master-practice/d' ~/.ssh/known_hosts")
+	os.system("sed -i '' '/slave-practice/d' ~/.ssh/known_hosts")
+
+	return "known_hosts information deleted."
+
+
+# locked_long_query
+@app.get('/lumos/locked_long_query_check', tags=["locked_long_query_check"])
+def locked_long_query_check():
+	now = datetime.datetime.today()
+	Log = Logger("./logs/{}.log".format(now), 0)
+	Log.write_log(3, "[LUMOS][Report] Call Received\n")	
+	dbconn = MysqlConnector('slave-practice.ay1.krane.9rum.cc', 3306, 'analysis', 'mha', 'mha')
+
+	# blocking query(롱쿼리)가 시작된 시간 조회
+	query="select trx_started from information_schema.innodb_trx where trx_state='RUNNING' and trx_query is null order by trx_started;"
+	ret=dbconn.execsql(query)[0][0]
+	now=datetime.datetime.today()
+	if now-ret>datetime.timedelta(seconds=100):
+		return 0
+	return 1
+
+
 if __name__ == '__main__':
 	print("================= LUMOS Start : {} =================".format(datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")))
 	uvicorn.run(app, host="0.0.0.0", debug=True, port=4321)
+	# uvicorn.run(app, host="192.168.0.17", debug=True, port=4321)
 	print("================= LUMOS End : {} =================".format(datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")))
